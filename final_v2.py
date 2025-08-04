149952e91af11ce41c5e0e672e95f5fb540a0954
Milan Rakic <milan.p.rakic@gmail.com>
2025-08-04 22:23:05 +0100

    forgot to add comment

--- final_v2.py
+++ final_v2.py
@@ -2,8 +2,10 @@ import os
 import sys
 from attack_v2 import generate_adversarial_image
 
+# Please replace image path with your own image path
 image_path = "n01440764_tench.JPEG"
 
+# likewise with label
 target_label = 'stingray'
 
 generate_adversarial_image(image_path, target_label) 
